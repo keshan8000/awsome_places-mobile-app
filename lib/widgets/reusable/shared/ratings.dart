cd0fb73c7046c6584c90dff23bca728a18afed33
keshan8000 <124331848+keshan8000@users.noreply.github.com>
2025-05-03 14:23:25 +0530

    awsome places
    practice flutter using images

---
+++ lib/widgets/reusable/shared/ratings.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Ratings extends StatelessWidget {
+  const Ratings({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        // ignore: deprecated_member_use
+        color: const Color(0xffCACACA).withOpacity(0.3),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const Icon(Icons.star, size: 40, color: Color(0xffFFE500)),
+          const Icon(Icons.star, size: 40, color: Color(0xffFFE500)),
+          const Icon(Icons.star, size: 40, color: Color(0xffFFE500)),
+          const Icon(Icons.star, size: 40, color: Color(0xffFFE500)),
+          Icon(
+            Icons.star,
+            size: 40,
+            // ignore: deprecated_member_use
+            color: const Color(0xff3B3636).withOpacity(0.75),
+          ),
+        ],
+      ),
+    );
+  }
+}
